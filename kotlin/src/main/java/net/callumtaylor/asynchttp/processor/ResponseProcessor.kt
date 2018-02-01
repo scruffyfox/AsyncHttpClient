1db0294ceea491bd949bb37d22bad7428e7356ea
Callum Taylor <callum@callumtaylor.net>
2018-02-01 15:03:08 +0000

    Updated response processor base methods

--- kotlin/src/main/java/net/callumtaylor/asynchttp/processor/ResponseProcessor.kt
+++ kotlin/src/main/java/net/callumtaylor/asynchttp/processor/ResponseProcessor.kt
@@ -2,18 +2,20 @@ package net.callumtaylor.asynchttp.processor
 
 import android.support.annotation.UiThread
 import android.support.annotation.WorkerThread
-import net.callumtaylor.asynchttp.obj.Packet
 import net.callumtaylor.asynchttp.obj.Request
 import java.io.InputStream
 
 /**
  * // TODO: Add class description
  */
-interface ResponseProcessor<T>
+abstract class ResponseProcessor<out T>
 {
 	@UiThread
-	fun onChunkProcessed(request: Request, length: Long, total: Long)
+	open fun onRequest(){}
+
+	@UiThread
+	open fun onChunkProcessed(request: Request, length: Long, total: Long){}
 
 	@WorkerThread
-	fun processStream(inputStream: InputStream, contentLength: Long, progressCallback: (Long, Long) -> Unit): T
+	abstract fun processStream(inputStream: InputStream, contentLength: Long, progressCallback: (Long, Long) -> Unit): T
 }
